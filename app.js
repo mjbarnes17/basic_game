cf7e61b760f5c0dfe39e61d190d86ab27680ea42
Marcus Barnes <mavjbarnes@gmail.com>
2015-08-29 01:15:01 -0400

    added paddle and controls

--- app.js
+++ app.js
@@ -8,7 +8,41 @@ var canvas = document.getElementById('canvas'),
     y = canvas.height - 30,
     dx = 1,
     dy = -1,
-    ballRadious = 5;
+    ballRadious = 5,
+    paddleHeight = 10,
+    paddleWidth = 50,
+    paddleX = (canvas.width - paddleWidth) / 2,
+    paddleY = canvas.height - paddleHeight,
+    rightPressed = false,
+    leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+var keyDownHandler = function(evnt) {
+    if (evnt.keyCode == 39) {
+        rightPressed = true;
+    } else if (envt.keyCode == 37) {
+        leftPressed = true;
+    }
+};
+
+var keyUpHandler = function(evnt) {
+    if (evnt.keyCode == 39) {
+        rightPressed = false;
+    } else if (envt.keyCode == 37) {
+        leftPressed = false;
+    }
+};
+
+// Draws paddle
+var drawPaddle = function() {
+  context.beginPath();
+  context.rect(paddleX, paddleY, paddleWidth, paddleHeight);
+  context.fillStyle = "pink";
+  context.fill();
+  context.closePath();
+};
 
 // Draws the ball
 var drawBall = function() {
@@ -23,18 +57,25 @@ var drawBall = function() {
 var draw = function() {
   context.clearRect(0, 0, width, height);
   drawBall();
-
-
+  drawPaddle();
+  // Reverses the value of ball position when it reaches the top or bottom of canvas window
   if (y + dy > height - ballRadious || y + dy < ballRadious) {
     dy = -dy;
   }
-
+  // Reverses the value of ball position when it reaches the left or right of canvas window
   if (x + dx > width - ballRadious || x + dx < ballRadious) {
     dx = -dx;
+  }
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
   }
   x += dx;
   y += dy;
 };
+
+
 
 // setInterval() fires the draws every 5 miliseconds
 setInterval(draw, 5);
